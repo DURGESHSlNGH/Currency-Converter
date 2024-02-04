016e7e1cd55f6eaa248be53dba2d7af82f3034a9
Durgesh Singh <durgeshssingh10@gmail.com>
2024-02-04 05:50:12 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,210 @@
+const countryList = {
+    AED: "AE",
+    AFN: "AF",
+    XCD: "AG",
+    ALL: "AL",
+    AMD: "AM",
+    ANG: "AN",
+    AOA: "AO",
+    AQD: "AQ",
+    ARS: "AR",
+    AUD: "AU",
+    AZN: "AZ",
+    BAM: "BA",
+    BBD: "BB",
+    BDT: "BD",
+    XOF: "BE",
+    BGN: "BG",
+    BHD: "BH",
+    BIF: "BI",
+    BMD: "BM",
+    BND: "BN",
+    BOB: "BO",
+    BRL: "BR",
+    BSD: "BS",
+    NOK: "BV",
+    BWP: "BW",
+    BYR: "BY",
+    BZD: "BZ",
+    CAD: "CA",
+    CDF: "CD",
+    XAF: "CF",
+    CHF: "CH",
+    CLP: "CL",
+    CNY: "CN",
+    COP: "CO",
+    CRC: "CR",
+    CUP: "CU",
+    CVE: "CV",
+    CYP: "CY",
+    CZK: "CZ",
+    DJF: "DJ",
+    DKK: "DK",
+    DOP: "DO",
+    DZD: "DZ",
+    ECS: "EC",
+    EEK: "EE",
+    EGP: "EG",
+    ETB: "ET",
+    EUR: "FR",
+    FJD: "FJ",
+    FKP: "FK",
+    GBP: "GB",
+    GEL: "GE",
+    GGP: "GG",
+    GHS: "GH",
+    GIP: "GI",
+    GMD: "GM",
+    GNF: "GN",
+    GTQ: "GT",
+    GYD: "GY",
+    HKD: "HK",
+    HNL: "HN",
+    HRK: "HR",
+    HTG: "HT",
+    HUF: "HU",
+    IDR: "ID",
+    ILS: "IL",
+    INR: "IN",
+    IQD: "IQ",
+    IRR: "IR",
+    ISK: "IS",
+    JMD: "JM",
+    JOD: "JO",
+    JPY: "JP",
+    KES: "KE",
+    KGS: "KG",
+    KHR: "KH",
+    KMF: "KM",
+    KPW: "KP",
+    KRW: "KR",
+    KWD: "KW",
+    KYD: "KY",
+    KZT: "KZ",
+    LAK: "LA",
+    LBP: "LB",
+    LKR: "LK",
+    LRD: "LR",
+    LSL: "LS",
+    LTL: "LT",
+    LVL: "LV",
+    LYD: "LY",
+    MAD: "MA",
+    MDL: "MD",
+    MGA: "MG",
+    MKD: "MK",
+    MMK: "MM",
+    MNT: "MN",
+    MOP: "MO",
+    MRO: "MR",
+    MTL: "MT",
+    MUR: "MU",
+    MVR: "MV",
+    MWK: "MW",
+    MXN: "MX",
+    MYR: "MY",
+    MZN: "MZ",
+    NAD: "NA",
+    XPF: "NC",
+    NGN: "NG",
+    NIO: "NI",
+    NPR: "NP",
+    NZD: "NZ",
+    OMR: "OM",
+    PAB: "PA",
+    PEN: "PE",
+    PGK: "PG",
+    PHP: "PH",
+    PKR: "PK",
+    PLN: "PL",
+    PYG: "PY",
+    QAR: "QA",
+    RON: "RO",
+    RSD: "RS",
+    RUB: "RU",
+    RWF: "RW",
+    SAR: "SA",
+    SBD: "SB",
+    SCR: "SC",
+    SDG: "SD",
+    SEK: "SE",
+    SGD: "SG",
+    SKK: "SK",
+    SLL: "SL",
+    SOS: "SO",
+    SRD: "SR",
+    STD: "ST",
+    SVC: "SV",
+    SYP: "SY",
+    SZL: "SZ",
+    THB: "TH",
+    TJS: "TJ",
+    TMT: "TM",
+    TND: "TN",
+    TOP: "TO",
+    TRY: "TR",
+    TTD: "TT",
+    TWD: "TW",
+    TZS: "TZ",
+    UAH: "UA",
+    UGX: "UG",
+    USD: "US",
+    UYU: "UY",
+    UZS: "UZ",
+    VEF: "VE",
+    VND: "VN",
+    VUV: "VU",
+    YER: "YE",
+    ZAR: "ZA",
+    ZMK: "ZM",
+    ZWD: "ZW",
+};
+
+const BASE_URL = "https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies";
+
+const dropDowns = document.querySelectorAll(".main select");
+const fromVal = document.querySelector(".from select");
+const toVal = document.querySelector(".to select");
+const button = document.querySelector(".btn");
+const message = document.querySelector(".msg");
+
+for (let dropDown of dropDowns) {
+    for (let currencyCode in countryList) {
+        let newOption = document.createElement("option");
+        newOption.innerText = currencyCode;
+        newOption.value = currencyCode;                          //value = object ke andar ke pehle wale values
+        dropDown.append(newOption);                              //value[_] = values ke jo keys h wo access honge
+    }
+    dropDown.addEventListener("click", (event) => {              //ye event syntax mei hi h toh ye lagana hi h
+        updateFlag(event.target)                                 //.target -> har ek values ko target karega
+     });
+}
+
+const updateFlag = (element) => {
+    currencyCode = element.value;
+    countryCode = countryList[currencyCode];
+    let newImg = element.parentElement.querySelector(".img");
+    let newSrc = `https://flagsapi.com/${countryCode}/flat/64.png`;
+    newImg.src = newSrc;
+}
+
+//async - await
+const conversion = async () => {
+    let amount = document.querySelector(".input input");         //andar isiley kiya kyunki jab bhi function call hoga
+    let amtVal = amount.value;                                   //tab tab ye values change hogi
+    if (amtVal === "" || amtVal < 1) {
+      amtVal = 1;
+      amount.value = "1";
+    }
+    const newURL = `${BASE_URL}/${fromVal.value.toLowerCase()}/${toVal.value.toLowerCase()}.json`;
+    let response = await fetch(newURL);
+    let data = await response.json();
+    let exchangeRates = data[toVal.value.toLowerCase()];
+    let finalAmount = amtVal * exchangeRates;
+    message.innerText = `${amtVal} ${fromVal.value} = ${finalAmount} ${toVal.value}`;
+}
+
+button.addEventListener("click", (event) => {
+    event.preventDefault();                                //preventDefault() -> jo bhi cheeze khud se hoti h usse 
+    conversion();                                          //                    prevent karta h
+  });
